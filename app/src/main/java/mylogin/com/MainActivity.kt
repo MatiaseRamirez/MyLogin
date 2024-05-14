7109c955219b56a0c98ec5e374383a35708523ef
MatiaseRamirez <matiasramirez333@hotmail.com>
2024-05-14 14:10:16 -0300

    boton de registro

--- app/src/main/java/mylogin/com/MainActivity.kt
+++ app/src/main/java/mylogin/com/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.util.Log
+import android.widget.TextView
 import com.example.activityhome.HomeActivity
 
 
@@ -17,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var usernameInput : EditText
     lateinit var passwordInput : EditText
     lateinit var loginBtn : Button
+    lateinit var twregister :  TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +28,15 @@ class MainActivity : AppCompatActivity() {
         usernameInput = findViewById(R.id.username_input)
         passwordInput = findViewById(R.id.password_input)
         loginBtn = findViewById(R.id.login_btn)
+        twregister=findViewById(R.id.register_link)
+
+
+        twregister.setOnClickListener {
+            val intent=Intent(this,RegistroActivity::class.java)
+            startActivity(intent)
+        }
+
+
 
         loginBtn.setOnClickListener{
             val username = usernameInput.text.toString()
@@ -36,10 +47,7 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+
     }
 }
